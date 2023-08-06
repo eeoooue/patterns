ae0d8826d4b98b8100b72d0a1ad10e8f035d46b5
eeoooue <pdmoorhouse@gmail.com>
2023-08-06 22:07:23 +0100

    created specific classes for the black & white pieces

---
+++ docs/abstract-factory/whitepieces.dart
@@ -0,0 +1,25 @@
+import 'chess.dart';
+
+class WhitePawn extends Pawn {
+  WhitePawn() : super("w") {}
+}
+
+class WhiteKnight extends Knight {
+  WhiteKnight() : super("w") {}
+}
+
+class WhiteBishop extends Bishop {
+  WhiteBishop() : super("w") {}
+}
+
+class WhiteRook extends Rook {
+  WhiteRook() : super("w") {}
+}
+
+class WhiteKing extends King {
+  WhiteKing() : super("w") {}
+}
+
+class WhiteQueen extends Queen {
+  WhiteQueen() : super("w") {}
+}
