f47343ecdc043f5419993813ba146303a4ed24da
eeoooue <pdmoorhouse@gmail.com>
2023-08-07 17:33:24 +0100

    drafted a KnightMovement strategy

--- docs/strategy/strategy.dart
+++ docs/strategy/strategy.dart
@@ -34,3 +34,24 @@ class PawnMovement implements MovementStrategy {
     return options;
   }
 }
+
+class KnightMovement implements MovementStrategy {
+  List<MoveOption> move(ChessBoard board, int i, int j) {
+    List<MoveOption> options = List.empty();
+
+    List<int> components = List.from({1, 2, -2, -1});
+
+    for (int a in components) {
+      for (int b in components) {
+        if (a.abs() + b.abs() == 3) {
+          MoveOption move = MoveOption(i + a, j + b);
+          if (board.tileIsEmpty(move.i, move.j)) {
+            options.add(move);
+          }
+        }
+      }
+    }
+
+    return options;
+  }
+}
