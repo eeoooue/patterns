7adbee24dfe57a5d5caf7fb3817c95f093e0e5f8
eeoooue <pdmoorhouse@gmail.com>
2023-08-07 17:06:03 +0100

    removed unused elements from abstract factory example

--- docs/strategy/script.dart
+++ docs/strategy/script.dart
@@ -1,59 +1,7 @@
 import 'dart:html';
 
 // game-specific classes
-import 'abstractfactory.dart';
 import 'chess.dart';
-
-class PieceBox {
-  ChessGame game;
-  Element container;
-  ButtonElement button;
-  ChessPieceFactory factory = BlackPieceFactory();
-  ChessPiece? selection = null;
-
-  PieceBox(this.game, this.container, this.button) {
-    armButton();
-    switchColour();
-  }
-
-  void armButton() {
-    button.addEventListener("click", (event) {
-      switchColour();
-    });
-  }
-
-  void switchColour() {
-    String current = factory.colour;
-    factory = (current == "black") ? WhitePieceFactory() : BlackPieceFactory();
-
-    button.classes.remove("${current}-btn");
-    button.classes.add("${factory.colour}-btn");
-    button.innerText = factory.colour;
-
-    presentPieces();
-  }
-
-  void presentPieces() {
-    container.children.clear();
-
-    for (ChessPiece piece in getOptions()) {
-      container.children.add(piece.element);
-    }
-  }
-
-  List<ChessPiece> getOptions() {
-    List<ChessPiece> options = List.empty(growable: true);
-
-    options.add(factory.createPawn());
-    options.add(factory.createBishop());
-    options.add(factory.createKnight());
-    options.add(factory.createRook());
-    options.add(factory.createQueen());
-    options.add(factory.createKing());
-
-    return options;
-  }
-}
 
 void main() {
   setupBox();
@@ -65,12 +13,5 @@ void setupBox() {
   if (gameContainer is Element) {
     ChessGame game = ChessGame(gameContainer);
     game.createBoard();
-
-    Element? switchBtn = document.querySelector(".colour-switcher");
-    Element? pieceContainer = document.getElementById("piece-box");
-
-    if (switchBtn is ButtonElement && pieceContainer is Element) {
-      PieceBox(game, pieceContainer, switchBtn);
-    }
   }
 }
