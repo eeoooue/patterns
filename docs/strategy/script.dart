762d06442bdc24563dc7e8e1a8230fe82cd1277b
eeoooue <pdmoorhouse@gmail.com>
2023-08-07 18:23:48 +0100

    demo now initializes with a checker on the board

--- docs/strategy/script.dart
+++ docs/strategy/script.dart
@@ -2,16 +2,23 @@ import 'dart:html';
 
 // game-specific classes
 import 'chess.dart';
+import 'strategy.dart';
 
 void main() {
-  setupBox();
+  setupDemo();
 }
 
-void setupBox() {
+void setupDemo() {
   Element? gameContainer = document.getElementById("game-container");
 
   if (gameContainer is Element) {
     ChessGame game = ChessGame(gameContainer);
-    game.createBoard();
+    game.startGame();
+
+    int i = 4;
+    int j = 3;
+
+    MimicPiece mimic = MimicPiece(i, j, PawnMovement());
+    game.board.placePiece(mimic, i, j);
   }
 }
