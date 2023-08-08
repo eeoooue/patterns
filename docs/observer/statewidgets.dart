458aa2052121d99d8e128dd0e7cb32971bd2ff71
eeoooue <pdmoorhouse@gmail.com>
2023-08-08 16:20:42 +0100

    created an AnalysisWidget

--- docs/observer/statewidgets.dart
+++ docs/observer/statewidgets.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 import 'chess.dart';
 import 'observer.dart';
 
@@ -75,5 +76,66 @@ class PlayerBlock {
     if (active == true) {
       element.classes.remove("inactive");
     }
+  }
+}
+
+class AnalysisWidget implements Observer {
+// <div class="container totempole" id="analysis-widget">
+//     <h3>Analysis</h3>
+//     <div class="container bookshelf" id="analysis-bar">
+//         <div class="teamclr-block white"></div>
+//         <div class="teamclr-block black"></div>
+//     </div>
+// </div>
+
+  late Element container;
+  late Element progressBar;
+  Random rand = Random();
+
+  AnalysisWidget(Subject game) {
+    container = createContainer();
+    container.children.add(createHeader());
+    container.children.add(createBookshelf());
+    game.subscribe(this);
+    // update(game);
+  }
+
+  Element createProgressBar() {
+    Element bar = document.createElement("div");
+    bar.classes.add("white-block");
+    return bar;
+  }
+
+  Element createHeader() {
+    Element header = document.createElement("h3");
+    header.innerText = "Analysis";
+    return header;
+  }
+
+  Element createBookshelf() {
+    Element bookshelf = document.createElement("div");
+    bookshelf.classes.add("container");
+    bookshelf.classes.add("bookshelf");
+    bookshelf.id = "analysis-bar";
+    progressBar = createProgressBar();
+    bookshelf.children.add(progressBar);
+    return bookshelf;
+  }
+
+  Element createContainer() {
+    Element container = document.createElement("div");
+    container.classes.add("container");
+    container.classes.add("totempole");
+    container.id = "analysis-widget";
+    return container;
+  }
+
+  String getRandPercentage() {
+    int amount = rand.nextInt(101);
+    return "${amount}%";
+  }
+
+  void update(Subject subject) {
+    progressBar.style.width = getRandPercentage();
   }
 }
