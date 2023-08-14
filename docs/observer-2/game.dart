d59e886f2d91df868d858f9fd1492e45895ff6e0
eeoooue <pdmoorhouse@gmail.com>
2023-08-14 19:12:59 +0100

    working on new layout version of observer demo

---
+++ docs/observer-2/game.dart
@@ -0,0 +1,43 @@
+import 'dart:html';
+
+abstract class Game {
+  void startGame();
+  void submitMove(int i, int j);
+  bool gameIsOver();
+}
+
+abstract class GameBoard {
+  void removePiece(int i, int j);
+  void setupPieces();
+  GamePiece getPiece(int i, int j);
+  void placePiece(GamePiece piece, int i, int j);
+  List<List<GamePiece>> getBoardState();
+}
+
+abstract class GameView {
+  void displayBoard(List<List<GamePiece>> boardstate);
+  Element buildTile(GamePiece piece);
+  void rotateBoard();
+}
+
+abstract class GamePiece {
+  late String src;
+  int i = 0;
+  int j = 0;
+
+  GamePiece() {}
+
+  void setSource(String address) {
+    src = address;
+  }
+
+  Element getElement() {
+    Element img = document.createElement("img");
+    img.classes.add("piece-img");
+    if (img is ImageElement) {
+      img.src = src;
+    }
+
+    return img;
+  }
+}
